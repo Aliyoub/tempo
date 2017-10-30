940c11f9d65e835512d5945c74229fe0519b260a
unknown <kibmimi>
2017-10-30 08:51:50 +0100

    premier commit

---
+++ vueJs/src/components/CreateItem2.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h1>Create An Item</h1>
+    <form>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Item Name:</label>
+            <input type="text" class="form-control" v-model="item.name">
+          </div>
+        </div>
+        </div>
+        <div class="form-group">
+          <button class="btn btn-primary">Valider</button>
+        </div>
+
+        <div class="col-md-6">
+          <div style="border:1px #eee solid">
+            {{item}}
+          </div>
+        </div><br />
+
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      item: 'czzzcc',
+      //items: []
+    }
+  },
+  methods: {
+    myFunction(){
+      //this.items.push({message: document.querySelector('input').value});
+      //this.item = '';
+    }
+
+    /*myFunction(){
+      this.remoteIpAdress = 'Loading...';
+      this.item = '';
+      axios.get('https://dns.google.com/resolve?name=' + url)
+      .then(function(response){
+        vm.remoteIpAdress = response.data.Answer[0].data;
+      })
+      .catch(function (error) {
+        vm.remoteIpAdress = 'error' + error;
+      });
+    }*/
+  }
+}
+</script>
